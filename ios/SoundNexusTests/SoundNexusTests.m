7d5e7544d51947fadb666b30af3f57cb8e640b61
Jose Abril Jr <jose.abril4@gmail.com>
2023-06-07 20:13:12 +0800

    Renamed

--- ios/SoundNexusTests/SoundNexusTests.m
+++ ios/SoundNexusTests/SoundNexusTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface webmintappTests : XCTestCase
+@interface SoundNexusTests : XCTestCase
 
 @end
 
-@implementation webmintappTests
+@implementation SoundNexusTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
